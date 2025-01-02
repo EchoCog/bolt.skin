1dcffeb7d50ca5dfe4357f0039db8151dfc1963d
drzo <d@rzo.io>
2025-01-02 17:37:28 +0200

    v11

---
+++ bindings.ps1
@@ -0,0 +1,15 @@
+$bindings = ""
+
+if (Test-Path .env.local) {
+    Get-Content .env.local | ForEach-Object {
+        $line = $_.Trim()
+        if ($line -and !$line.StartsWith("#")) {
+            $name, $value = $line -split "=", 2
+            $value = $value -replace '^"(.*)"$', '$1'
+            $bindings += "--binding ${name}=${value} "
+        }
+    }
+}
+
+$bindings = $bindings.TrimEnd()
+Write-Output $bindings
