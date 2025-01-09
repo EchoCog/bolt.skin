75b3e46248081474914028ae1cee92fc4ffc8490
drzo <d@rzo.io>
2025-01-09 02:27:31 +0200

    fix: configure vite plugins and add unocss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
+import UnoCSS from "unocss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { optimizeCssModules } from "vite-plugin-optimize-css-modules";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [
+    UnoCSS(),
+    remix(),
+    nodePolyfills(),
+    optimizeCssModules(),
+    tsconfigPaths()
+  ],
   build: {
     rollupOptions: {
       output: {
